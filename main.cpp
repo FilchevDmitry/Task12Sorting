199d1b17dc618193dba6d16a4a4d6f89ac916bf4
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-05 13:10:16 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"Input 15 fractional numbers\n";
+    float numbers[15];
+    float temp;
+    for (int i = 0; i < 15; ++i)
+    {
+        std::cin>>numbers[i];
+    }
+    for (int i = 0; i < 15; ++i)
+    {
+        for (int j = 1; j < 15; ++j)
+        {
+            if(numbers[j-1]<numbers[j])
+            {
+                temp=numbers[j-1];
+                numbers[j-1]=numbers[j];
+                numbers[j]=temp;
+            }
+        }
+    }
+    for (int i = 0; i < 15; ++i) {
+      std::cout<<numbers[i]<<" ";
+    }
     return 0;
 }
